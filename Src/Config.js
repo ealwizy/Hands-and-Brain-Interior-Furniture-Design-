fce0db8515a0abd044cb9e9635933e1aee164286
ealwizy <96386992+ealwizy@users.noreply.github.com>
2025-10-28 17:46:40 +0100

    Add application configuration settings

---
+++ Src/Config.js
@@ -0,0 +1,4 @@
+export const APP_CONFIG = {
+  appName: "Hands & Brain Interior Furniture Design",
+  backendURL: "https://hands-brain-server.up.railway.app", // paste your new Railway link here
+};
