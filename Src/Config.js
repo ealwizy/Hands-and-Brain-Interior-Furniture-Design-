ccad1bd98fe4412e451060655fe8154623c7a494
ealwizy <96386992+ealwizy@users.noreply.github.com>
2025-10-28 18:30:36 +0100

    Refactor APP_CONFIG to CONFIG with environment variables
    Updated configuration to use environment variables for API and Cloudinary settings.

--- Src/Config.js
+++ Src/Config.js
@@ -1,4 +1,6 @@
-export const APP_CONFIG = {
-  appName: "Hands & Brain Interior Furniture Design",
-  backendURL: "https://hands-brain-server.up.railway.app", // paste your new Railway link here
+export const CONFIG = {
+  API_BASE_URL: import.meta.env.VITE_API_URL || "https://handsbrain.up.railway.app",
+  CLOUDINARY_CLOUD_NAME:ds5ubvxru import.meta.env.VITE_CLOUDINARY_CLOUD_NAME || "your_cloud_name",
+  CLOUDINARY_API_KEY:677296215488994 import.meta.env.VITE_CLOUDINARY_API_KEY || "your_api_key",
+  CLOUDINARY_API_SECRET:aujzjSmSMrGkNoGadM1pk0fokO8 import.meta.env.VITE_CLOUDINARY_API_SECRET || "your_api_secret",
 };
