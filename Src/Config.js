fb503d2bda3b412174e05c1255254e4a7a84266d
ealwizy <96386992+ealwizy@users.noreply.github.com>
2025-10-28 19:08:31 +0100

    Update Cloudinary API key and secret in Config.js
    Updated Cloudinary API credentials in the configuration.

--- Src/Config.js
+++ Src/Config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   API_BASE_URL: import.meta.env.VITE_API_URL || "https://handsbrain.up.railway.app",
   CLOUDINARY_CLOUD_NAME:ds5ubvxru import.meta.env.VITE_CLOUDINARY_CLOUD_NAME || "your_cloud_name",
-  CLOUDINARY_API_KEY:677296215488994 import.meta.env.VITE_CLOUDINARY_API_KEY || "your_api_key",
-  CLOUDINARY_API_SECRET:aujzjSmSMrGkNoGadM1pk0fokO8 import.meta.env.VITE_CLOUDINARY_API_SECRET || "your_api_secret",
+  CLOUDINARY_API_KEY:792457115748986 import.meta.env.VITE_CLOUDINARY_API_KEY || "your_api_key",
+  CLOUDINARY_API_SECRET:eBHDxjA-4CHBQHPdP0DMjTXei_4 import.meta.env.VITE_CLOUDINARY_API_SECRET || "your_api_secret",
 };
